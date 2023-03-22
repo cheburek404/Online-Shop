42cdcf85ade149a56b29bd63dca3d17da622d26f
Azat Bakyt <azatbakyt8@gmail.com>
2023-03-22 17:54:37 +0600

    got data from API and work with Vuex store

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -3,7 +3,7 @@
         <h1>Catalog</h1>
         <div class="catalog__list">
             <catalog-item 
-                v-for="product in products"
+                v-for="product in PRODUCTS"
                 :key="product.article"
                 :product_data="product"
                 @sendDataToParent="showArticles"
@@ -14,6 +14,7 @@
 
 <script>
 import CatalogItem from './CatalogItem.vue';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: "Catalog",
@@ -22,56 +23,29 @@ export default {
     },
     data() {
         return {
-            products: [
-                {
-                    image: "1.jpg",
-                    name: "T-shirt 1",
-                    price: 2100,
-                    article: "T1",
-                    available: true
-                },
-                {
-                    image: "2.png",
-                    name: "T-shirt 2",
-                    price: 3150,
-                    article: "T2",
-                    available: true
-                },
-                {
-                    image: "3.jpg",
-                    name: "T-shirt 3",
-                    price: 4200,
-                    article: "T3",
-                    available: false
-                },
-                {
-                    image: "4.jpg",
-                    name: "T-shirt 4",
-                    price: 5300,
-                    article: "T4",
-                    available: true
-                },
-                {
-                    image: "5.jpg",
-                    name: "T-shirt 5",
-                    price: 6500,
-                    article: "T5",
-                    available: false
-                },
-                {
-                    image: "6.jpg",
-                    name: "T-shirt 6",
-                    price: 8700,
-                    article: "T6",
-                    available: true
-                }
-            ]
+            
         }
     },
+    computed: {
+        ...mapGetters([
+            'PRODUCTS'
+        ]),
+    },
     methods: {
+        ...mapActions([
+            'GET_PRODUCTS_FROM_API'
+        ]),
         showArticles(data) {
             console.log(data);
-        }
+        },
+    },
+    mounted() {
+        this.GET_PRODUCTS_FROM_API()
+        .then((response) => {
+            if (response.data) {
+                console.log('Data arrived!')
+            }
+        })
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
+import axios from 'axios';
 import { createApp } from 'vue'
+import { createStore } from "vuex";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const store = createStore({
+    state: {
+        products: []
+    },
+    mutations: {
+        SET_PRODUCTS_TO_STATE: (state, products) => {
+            state.products = products;
+        }
+    },
+    actions: {
+        GET_PRODUCTS_FROM_API({commit}) {
+            return axios
+                .get('http://localhost:3000/products')
+                .then((products) => {
+                    commit('SET_PRODUCTS_TO_STATE', products.data);
+                    return products;
+                })
+                .catch((error) => {
+                    console.log(error);
+                    return error;
+                })
+        }
+    },
+    getters: {
+        PRODUCTS(state) {
+            return state.products;
+        }
+    },
+}); 
+
+createApp(App).use(store).mount('#app')
